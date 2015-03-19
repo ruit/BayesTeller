fa31a0748c968402987db4f2f0af8241e984df0f
ruit <tianremi@gmail.com>
2015-03-18 19:18:09 -0500

    modular

--- Feb2015_pool_everything/bayesTellerFeb27_2015.py
+++ Feb2015_pool_everything/bayesTellerFeb27_2015.py
@@ -2,7 +2,7 @@
 
 #Tian R. <tianremi@gmail.com>
 #Feb 27, 2015 
-#March 18, 2015
+#March 18b, 2015
 
 import sys, gzip, re
 import math
@@ -11,7 +11,7 @@ import math
 
 '''
 
-if len(sys.argv)<5:
+if len(sys.argv)<6:
 	raise SystemExit("I need arguments!")
 
 
@@ -20,7 +20,8 @@ try:
 	sampleFile=sys.argv[2] #unknow sample of SNVs, gzipped
 	germFreq=sys.argv[3] # freq in panCancer germ trained
 	tumorFreq=sys.argv[4] # freq in panCancer tumor trained
-	cutoff=float(sys.argv[5]) #take top 0.3 (30%) for sum of the snvlogRatio
+	sampleSize=float(sys.argv[5]) # 3909 or half, March 18, 2015
+	cutoff=float(sys.argv[6]) #take top 0.3 (30%) for sum of the snvlogRatio
 	cutoff=math.log10(cutoff)
 	#print sys.argv
 
@@ -71,19 +72,30 @@ for line in open(tumorFreq, "r"):
 
 
 logR=[]
+logR_somaLike=[]
+logR_germLike=[]
+
 if len(hit)==0:
 	logR=[]
+	logR_somaLike=[]
+	logR_germLike=[]	
 else:
 
 	#March 18, 2015, if  not hits, assign zero
 	logR=[math.log10((0.5+Dict_freq_tumor_A.get(snv, 0))/(0.5+Dict_freq_germ_A.get(snv, 0))) \
 for snv in hit] # zero!
 	
+	for snv in hit:
+		if Dict_freq_germ_A.get(snv, 0) <= (sampleSize * 0.01): #1% as the cutoff, March 18, 2015	
+			logR_somaLike.append(math.log10((0.5+Dict_freq_tumor_A.get(snv, 0))/(0.5+Dict_freq_germ_A.get(snv, 0))))# zero!
+		else:
+			logR_germLike.append(math.log10((0.5+Dict_freq_tumor_A.get(snv, 0))/(0.5+Dict_freq_germ_A.get(snv, 0))))# zero!
 
 
-if len(logR)==0:
-	print "0"+"\t"+"NA"
-else:
+
+
+
+def pred(hit, all_logR, logR, cutoff, sampleFile, suffix):
 	keyR=[]
 	countedLoci=[]
 	l=len(logR)
@@ -91,16 +103,30 @@ else:
 	for ratio in logR:
 		if ratio>=cutoff:
 			keyR.append(ratio)
-			countedLoci.append(hit[logR.index(ratio)]) #March 6, 2015, keep record of counted top SNVs
+			countedLoci.append(hit[all_logR.index(ratio)]) #March 6, 2015, keep record of counted top SNVs
 
 	#keyR=logR[0:int(topR*l)] # take top 30% hits
-	if len(keyR)==0: print "0_0"+"\t"+"NA"
+	
+	fh_out=open(sampleFile+suffix, "w")
+	if len(keyR)==0: fh_out.write("0_0"+"\t"+"NA"+"\n")
 	else:
 	
 		val=reduce(lambda x,y: x+y, keyR)
 	#val=float(val/(l*topR))
-		print str(l)+"_"+str(len(keyR))+":"+";".join(countedLoci)+"\t"+str(val/l)
+		fh_out.write(str(l)+"\t"+str(len(keyR))+"\t"+str(val/l)+"\t"+";".join(countedLoci)+"\n")
+#":"+";".join(countedLoci)+
+
 		keyR=[]
+	fh_out.close()
 
 
 
+if len(logR)==0:
+	fh_out=open(sampleFile+".all", "w")
+	fh_out("0"+"\t"+"NA"+"\n")
+	fh_out.close()
+else:
+
+	pred(hit, logR, logR, cutoff, sampleFile, ".all")
+	pred(hit, logR, logR_somaLike, cutoff, sampleFile, ".sml")
+	pred(hit, logR, logR_germLike, cutoff, sampleFile, ".gml")
